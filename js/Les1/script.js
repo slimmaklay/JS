9b77085cf652560435e1ce57197a32fbcc0ca58d
slimmaklay <162340446+slimmaklay@users.noreply.github.com>
2024-06-29 16:10:18 +0300

    Add files via upload

---
+++ js/Les1/script.js
@@ -0,0 +1,61 @@
+// Якщо змінна більше нуля - виведіть true, менше - false
+//Перевірте це на варіантах 1, 0, -3.
+//let result = 1 > 0;
+//alert(result);
+//let result = 0 > 0;
+//alert(result);
+// let result = -3 > 0;
+// alert(result);
+
+// Якщо змінна ="test" - виведіть true,
+//Перевірте це на варіантах 'test', "qwerty", true
+// let result = "test";
+// alert(result == "test");
+// alert(result == "qwerty");
+// alert(result == true);
+
+// Якщо змінна більше 10 - відніміть 5,
+//менше - додайте 5, результат виведіть в консоль
+//Перевірте це на варіантах 1, 10, 13.
+// let number = 1;
+// if (number > 10) {
+//   console.log(number - 5);
+// } else {
+//   console.log(number + 5);
+// }
+
+//Зробіть сервіс який отримує число від 1 до 12
+// виведіть місяць який дорівнює числу
+// let randomMonth = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+// if (randomMonth === 1) {
+//   alert("Січень");
+// } else if (randomMonth === 2) {
+//   alert("Лютий");
+// } else if (randomMonth === 3) {
+//   alert("Березень");
+// } else if (randomMonth === 4) {
+//   alert("Квітень");
+// } else if (randomMonth === 5) {
+//   alert("Травень");
+// } else if (randomMonth === 6) {
+//   alert("Червень");
+// } else if (randomMonth === 7) {
+//   alert("Липень");
+// } else if (randomMonth === 8) {
+//   alert("Серпень");
+// } else if (randomMonth === 9) {
+//   alert("Вересень");
+// } else if (randomMonth === 10) {
+//   alert("Жовтень");
+// } else if (randomMonth === 11) {
+//   alert("Листопад");
+// } else if (randomMonth === 12) {
+//   alert("Грудень");
+// }
+
+//Зробіть сервіс який отримує тризначне число
+//Поверніть користувачу сумму цих чисел
+// let num1 = +prompt("Введіть перше число");
+// let num2 = +prompt("Введіть друге число");
+// let num3 = +prompt("Введіть третє число");
+// alert(num1 + num2 + num3);
